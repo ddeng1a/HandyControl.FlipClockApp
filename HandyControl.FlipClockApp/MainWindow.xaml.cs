57754f85a88ebe3b5b66ede634235709cfdf91c9
robot <robot@major.com>
2025-06-04 17:46:57 +0800

    添加项目文件。

---
+++ HandyControl.FlipClockApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media.TextFormatting;
+
+namespace HandyControl.FlipClockApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ HandyControl.FlipClockApp/Model/WidgetSettings.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HandyControl.FlipClockApp.Model
+{
+    public partial class WidgetSettings : ObservableObject
+    {
+        [ObservableProperty]
+        private int _windowTop;
+        [ObservableProperty]
+        private int _windowLeft;
+    }
+}
